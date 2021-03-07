b932ee5f75986bf8963f9df806ed4f61e0689e16
laurenkan. <laurenkan.@ayrens-air.attlocal.net>
2021-03-07 14:28:14 -0800

    5/7/2021 going back to school on tuesday! finally :D

--- src/_04_popcorn/PopcornMaker.java
+++ src/_04_popcorn/PopcornMaker.java
@@ -3,12 +3,20 @@ package _04_popcorn;
 import javax.swing.JOptionPane;
 
 public class PopcornMaker {
+	/* Your mission and you have to accept it:
+	 * Create a PopcornMaker class (in the popcorn package) and add a main method to it. 
+	 * The main method should create a bag of Popcorn and cook it in the microwave.
+	 * Ask the user for the flavor of the popcorn and the number of minutes to cook it.
+	 * Don't change the existing methods.
+	 */
 public static void main(String[] args) {
 	Microwave popcornBag = new Microwave();
-JOptionPane.showMessageDialog(null, "What flavor popcorn do you want?");
-String cookTimeinMinutes = JOptionPane.showInputDialog("How long should I cook the popcorn (minutes)?");
-popcornBag.setTime(cookTimeInMinutes);
-///AHHH OK NEXT CLASS FIX THIS UWUWUWUUEUTUWTUURT
-
+	String flavor = JOptionPane.showInputDialog(null, "What flavor popcorn do you want?");
+	Popcorn thingToBeCooked = new Popcorn(flavor);
+String cookTimeInMinutes = JOptionPane.showInputDialog("How long should I cook the popcorn (minutes)?");
+popcornBag.setTime(Integer.parseInt(cookTimeInMinutes));
+popcornBag.putInMicrowave(thingToBeCooked);
+popcornBag.startMicrowave();
+JOptionPane.showInputDialog("How is the popcorn? :)");
 }
 }
---
+++ src/_05_vault/JamesBond.java
@@ -0,0 +1,11 @@
+package _05_vault;
+
+public class JamesBond {
+public static void main(String[] args) {
+//ask teacher: "My instructions say: “James Bond is trying to crack a vault. Before we start to write code, "
+//"we will draw a UML diagram on the whiteboard (teacher will help) and figure out what the method "
+//"signatures will be.”" next class since there's no time
+
+
+}
+}
