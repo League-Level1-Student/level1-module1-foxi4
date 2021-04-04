c3b3810ce5836e9e79b04f54a7277b771bac1262
laurenkan. <laurenkan.@localhost>
2021-04-04 14:29:44 -0700

    4/4/21 yay almost done with module 1 level 1!

--- src/_99_extra/Pig.java
+++ src/_99_extra/Pig.java
@@ -1,5 +1,55 @@
 package _99_extra;
 
+import javax.swing.JOptionPane;
+
 public class Pig {
 
-}
+	    private String name;
+	    private boolean isSheepHerder;
+	     
+	    public Pig(String name, boolean canHerdSheep) {   //  write code here
+//1. finish pig constructor
+//2. instantiate two pigs, "Babe" and "Rasher" (i guess the pigs??)
+//3. Imagine there is a pig instance called ziggy. 
+//Write code that will print “bacon for breakfast” if ziggy is destined to be turned into bacon, 
+//or “eggs for breakfast” if he is not. 
+	    	this.name = name;
+	    	this.isSheepHerder = canHerdSheep;
+	   
+//left hand is what you're assigning to and right hand is what you're assigning to it.	    	
+	    }
+
+	    public boolean willBecomeBacon() {
+	        if(this.isSheepHerder) {
+	        	System.out.println("bacon for breakfast!");
+	            return false;
+	        }
+	        else {
+	        	System.out.println("eggs for breakfast～");
+	            return true;
+	        }
+	        //if true, bacon for breakfast, false it is eggs
+	    }
+	    public static void main(String[] args) {
+			Pig pig = new Pig("Babe", false );
+			Pig pig2 = new Pig ("Rasher", true);
+								   //can be true if you want the pig to be bacon >:D\\
+			pig.willBecomeBacon();
+			pig2.willBecomeBacon();
+		}
+	    //i think you can't do anything with this either just having it?
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/_99_extra/Practice.java
@@ -0,0 +1,15 @@
+package _99_extra;
+
+public class Practice {
+//INSTRUCTIONS
+	//TOPIC: Color
+	//Setup: need privates for each constructor?/. make a public that has "Name"(String l,Boolean o, Char 2, idk one of those).
+	//this."thing"="thinge"... maybe put in boolean (true or false), you know things that will make an actual thing happen
+	//make a runner in the same class (put a main out of the parenthesis of the constructor. make sure to call the variables for it to work
+	//1. make constructor
+		//make the public including (string, boolean, etc. stuff like that)
+	//2. runner (main)
+	//3. in main method, do the Practice (practice because that is the class name, can change), (String, boolean, etc. thing)
+	//4. call the things
+ //btw i moved the outliner (outlier??) and task down for more space
+}
